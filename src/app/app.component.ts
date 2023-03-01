8e88b2c2843de7a3fb279a474003629eb2cda1ce
vitorbda <80553392+vitorbda@users.noreply.github.com>
2023-03-01 06:38:48 -0300

    Seção 6: [Angular] - Module Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,14 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `          
+  template: `     
+  <!--     
   <app-diretivas-atributos>
     <h1>Aulas de Diretivas e atributos </h1>
     <h3> Final da aula </h3>
   </app-diretivas-atributos>
-  <app-diretivas-estruturais></app-diretivas-estruturais>
+  <app-diretivas-estruturais></app-diretivas-estruturais> -->
+  <app-new-component></app-new-component>
   <router-outlet></router-outlet>          
   `
 })
