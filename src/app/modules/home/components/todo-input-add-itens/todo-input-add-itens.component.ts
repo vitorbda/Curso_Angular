7dd270ec06afa68637cf8eab956c567d9889c158
vitorbda <80553392+vitorbda@users.noreply.github.com>
2023-03-07 06:50:54 -0300

    To-Do List [Pt. 7]

--- src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
+++ src/app/modules/home/components/todo-input-add-itens/todo-input-add-itens.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-todo-input-add-itens',
   templateUrl: './todo-input-add-itens.component.html',
   styleUrls: ['./todo-input-add-itens.component.scss']
 })
-export class TodoInputAddItensComponent {
+export class TodoInputAddItensComponent implements OnInit{
+  ngOnInit(): void {
+  }
+
+  
 
 }
--- src/app/modules/home/components/todo-list/todo-list.component.ts
+++ src/app/modules/home/components/todo-list/todo-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskList } from '../../model/task-list';
 
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
   styleUrls: ['./todo-list.component.scss']
 })
-export class TodoListComponent {
+export class TodoListComponent implements OnInit{
+  ngOnInit(): void {
+  }
 
+  constructor(){}
+
+  public taskList: Array<TaskList> = [
+    {task: "Minha task 1", checked: true},
+    {task: "minha task 2", checked: false}
+  ];
 }
